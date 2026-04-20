de10974bbc242ec8a5c0996387583614374f54ae
User <User@ANDRESMENDOZA>
2026-04-20 17:31:06 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_SkyInit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
