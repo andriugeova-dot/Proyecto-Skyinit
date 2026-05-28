cc1b67881941e7bfbe15aa1c5e6763e3de477075
jeffryrodriguez12-collab <jeffryrodriguez117@gmail.com>
2026-05-28 16:41:50 -0500

    vista de UsuariosMantenimientos

---
+++ Controllers/UsuariosMantenimientosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_SkyInit.Controllers
+{
+    public class UsuariosMantenimientosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
