6d530bd30a443044464a935612c41f1e9b75c217
Jhoan Cotamo <jhoan.cotamo42@gmail.com>
2026-06-01 16:06:22 -0500

    Visualizacion logueo de usuario

---
+++ Controllers/PanelUsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_SkyInit.Controllers
+{
+    public class PanelUsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
